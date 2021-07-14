4cda8db677c13c9fb2ee1e614744d90982c1ba50
Frederik Mathiesen <frederik_mathies@hotmail.com>
2021-07-14 11:04:14 +0200

    Fix profile layout on team page

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container-fluid">
     <div class="row p-4">
-        <div class="col-lg-3 col-md-4">
+        <div class="col-lg-4 col-md-6">
             @include('admin.components.profile')
         </div>
     </div>
