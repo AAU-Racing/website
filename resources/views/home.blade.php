5380f10cc4891b98d69a0c4f660729e3b938a3b8
Frederik Mathiesen <frederik_mathies@hotmail.com>
2021-08-31 13:41:05 +0200

    Temporary home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,18 @@
 @section('content')
 <x-carousel></x-carousel>
 <div class="container page-padding">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-           {!! $page['html'] !!}
+    <div class="row">
+        <div class="col-sm-9">
+            <div class="page-header">
+                <h1>{{ $page->title }}</h1>
+                <hr />
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-9">
+            {!! $page->content !!}
         </div>
     </div>
 </div>
